d7fe2477c0f67b6a82b3cd18ce536843dac623d8
Asma06754 <asmakismoune@yahoo.fr>
2024-07-05 16:49:12 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,64 @@
+info.onScore(100, function () {
+    game.gameOver(true)
+})
+scene.onHitWall(SpriteKind.Food, function (sprite, location) {
+    sprites.destroy(sprite, effects.disintegrate, 100)
+    info.changeLifeBy(-1)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    sprites.destroy(otherSprite, effects.fire, 100)
+    info.changeScoreBy(1)
+})
+let Pizza: Sprite = null
+game.splash("Abderrahmen eat the pizza", "Makecode Tutorial Game")
+tiles.setCurrentTilemap(tilemap`level2`)
+let Abderrahmen = sprites.create(img`
+    . . . . . . f f f f . . . . . . 
+    . . . . f f e e e e f f . . . . 
+    . . . f e e e f f e e e f . . . 
+    . . f f f f f 2 2 f f f f f . . 
+    . . f f e 2 e 2 2 e 2 e f f . . 
+    . . f e 2 f 2 f f 2 f 2 e f . . 
+    . . f f f 2 2 e e 2 2 f f f . . 
+    . f f e f 2 f e e f 2 f e f f . 
+    . f e e f f e e e e f e e e f . 
+    . . f e e e e e e e e e e f . . 
+    . . . f e e e e e e e e f . . . 
+    . . e 4 f f f f f f f f 4 e . . 
+    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+    . . 4 4 f 4 4 4 4 4 4 f 4 4 . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . f f . . f f . . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(Abderrahmen, 150, 0)
+Abderrahmen.setVelocity(80, 104)
+info.setScore(0)
+info.setLife(7)
+forever(function () {
+    for (let index = 0; index < 3; index++) {
+        music.play(music.stringPlayable("C5 B A A G F F E ", 120), music.PlaybackMode.UntilDone)
+    }
+    music.play(music.stringPlayable("E D C E D C E D ", 120), music.PlaybackMode.UntilDone)
+})
+game.onUpdateInterval(500, function () {
+    Pizza = sprites.create(img`
+        . . . . . . b b b b . . . . . . 
+        . . . . . . b 4 4 4 b . . . . . 
+        . . . . . . b b 4 4 4 b . . . . 
+        . . . . . b 4 b b b 4 4 b . . . 
+        . . . . b d 5 5 5 4 b 4 4 b . . 
+        . . . . b 3 2 3 5 5 4 e 4 4 b . 
+        . . . b d 2 2 2 5 7 5 4 e 4 4 e 
+        . . . b 5 3 2 3 5 5 5 5 e e e e 
+        . . b d 7 5 5 5 3 2 3 5 5 e e e 
+        . . b 5 5 5 5 5 2 2 2 5 5 d e e 
+        . b 3 2 3 5 7 5 3 2 3 5 d d e 4 
+        . b 2 2 2 5 5 5 5 5 5 d d e 4 . 
+        b d 3 2 d 5 5 5 d d d 4 4 . . . 
+        b 5 5 5 5 d d 4 4 4 4 . . . . . 
+        4 d d d 4 4 4 . . . . . . . . . 
+        4 4 4 4 . . . . . . . . . . . . 
+        `, SpriteKind.Food)
+    Pizza.setPosition(randint(10, 145), 10)
+    Pizza.setVelocity(0, 50)
+})
